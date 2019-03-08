e842e65f14b824fe8545e44d1e071b52175eeb4b
Jeffrey Dill <jeffdill2@gmail.com>
2019-03-08 14:44:26 -0500

    Implement functionality for multipart HTTP file uploads ammoready/ammoready-two-point-oh#442

--- lib/credova/api.rb
+++ lib/credova/api.rb
@@ -9,31 +9,74 @@ module Credova
 
     USER_AGENT = "CredovaRubyGem/#{Credova::VERSION}".freeze
 
+    FILE_UPLOAD_ATTRS = {
+      permitted: %i( file_name file_type file_contents ).freeze,
+      reqired:   %i( file_type file_contents ).freeze,
+    }
+
     def get_request(endpoint, headers = {})
       request = Net::HTTP::Get.new(request_url(endpoint))
 
-      process_request(request, {}, headers)
+      submit_request(request, {}, headers)
     end
 
     def post_request(endpoint, data = {}, headers = {})
       request = Net::HTTP::Post.new(request_url(endpoint))
 
-      process_request(request, data, headers)
+      submit_request(request, data, headers)
+    end
+
+    def post_file_request(endpoint, file_data, headers = {})
+      request = Net::HTTP::Post.new(request_url(endpoint))
+
+      submit_file_request(request, file_data, headers)
     end
 
     private
 
-    def process_request(request, data, headers)
+    def submit_request(request, data, headers)
       set_request_headers(request, headers)
 
-      uri = URI(request.path)
       request.body = data.is_a?(Hash) ? data.to_json : data
+
+      process_request(request)
+    end
+
+    def submit_file_request(request, file_data, headers)
+      boundary = ::SecureRandom.hex(15)
+
+      headers.merge!(content_type_header("multipart/form-data; boundary=#{boundary}"))
+
+      build_multipart_request_body(request, file_data, boundary)
+      set_request_headers(request, headers)
+      process_request(request)
+    end
+
+    def process_request(request)
+      uri = URI(request.path)
 
       response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
         http.request(request)
       end
 
       Credova::Response.new(response)
+    end
+
+    def build_multipart_request_body(request, file_data, boundary)
+      file_type     = file_data[:file_type]
+      file_contents = file_data[:file_contents]
+      file_name     = file_data[:file_name] || "ffl-document.#{file_type}"
+      content_type  = "application/#{file_data[:file_type]}"
+
+      body = []
+      body << "--#{boundary}\r\n"
+      body << "Content-Disposition: form-data; name=\"file\"; filename=\"#{file_name}\"\r\n"
+      body << "Content-Type: #{content_type}\r\n"
+      body << "\r\n"
+      body << "#{file_contents}\r\n"
+      body << "--#{boundary}--\r\n"
+
+      request.body = body.join
     end
 
     def set_request_headers(request, headers)
@@ -56,14 +99,6 @@ module Credova
         API_VERSION,
         endpoint,
       ].join('/')
-    end
-
-    def extract_file_extension(ffl_document_url)
-      metadata_raw  = Net::HTTP.get(URI.parse("#{ffl_document_url}/metadata"))
-      metadata_json = JSON.parse(metadata_raw)
-      file_name     = metadata_json['filename']
-
-      file_name[(file_name.rindex('.') + 1)..-1]
     end
 
     def standardize_body_data(submitted_data, permitted_data_attrs)
